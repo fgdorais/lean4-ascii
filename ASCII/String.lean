0e8b8211e8395bc368e111ac2355d7c06ef30947
F. G. Dorais <fgdorais@gmail.com>
2024-01-18 23:26:00 -0500

    feat: implement opaque definition

--- ASCII/String.lean
+++ ASCII/String.lean
@@ -14,10 +14,13 @@ structure String where
 
 namespace String
 
-/-- Empty string -/
-def empty : ASCII.String where
-  toByteArray := .empty
+/-- Empty string with specific capacity -/
+def mkEmpty (size : Nat) : ASCII.String where
+  toByteArray := .mkEmpty size
   valid := by intros; contradiction
+
+/-- Empty string -/
+abbrev empty : ASCII.String := mkEmpty 0
 
 instance : Inhabited String where
   default := .empty
@@ -27,38 +30,6 @@ abbrev length (s : ASCII.String) := s.toByteArray.size
 
 instance : GetElem String Nat Char fun s i => i < s.length where
   getElem s i h := ⟨s.toByteArray.get ⟨i, h⟩, s.valid i⟩
-
-/-- Coercion from ASCII string to Unicode string -/
-@[coe, extern "lean_string_from_utf8_unchecked"]
-def toUnicode (s : @&ASCII.String) : Unicode.String :=
-  loop 0 (Nat.zero_le _) ""
-where
-  loop (i : Nat) (hi : i ≤ s.length) (r : Unicode.String) :=
-    if h : i = s.length then r else
-      have hi : i < s.length := Nat.lt_of_le_of_ne hi h
-      loop (i + 1) (Nat.succ_le_of_lt hi) (r.push s[i])
-termination_by loop => s.length - i
-
-instance : Coe ASCII.String Unicode.String where
-  coe := String.toUnicode
-
-instance : ToString ASCII.String where
-  toString s := s.toUnicode
-
-/-- Coerce a Unicode string into an ASCII string -/
-@[extern "lean_string_to_utf8"]
-opaque ofUnicode (s : @&Unicode.String) (h : s.isASCII) : ASCII.String
-alias _root_.String.toASCII := ofUnicode
-
-@[inherit_doc String.ofUnicode]
-def ofUnicode? (s : Unicode.String) : Option ASCII.String :=
-  if h : s.isASCII then some (.ofUnicode s h) else none
-alias _root_.String.toASCII? := ofUnicode?
-
-@[inherit_doc String.ofUnicode]
-def ofUnicode! (s : Unicode.String) : ASCII.String :=
-  if h : s.isASCII then .ofUnicode s h else panic! "characters out of ASCII range"
-alias _root_.String.toASCII! := ofUnicode!
 
 /-- Append a character `c` at the end of string `s` -/
 def push (s : String) (c : Char) : String where
@@ -95,6 +66,46 @@ def append (s t : String) : String where
 def extract (s : String) (start stop : Nat) : String where
   toByteArray := s.toByteArray.extract start stop
   valid i h := by rw [ByteArray.get_extract]; exact s.valid (start + i)
+
+/-- Coercion from ASCII string to Unicode string -/
+@[coe, extern "lean_string_from_utf8_unchecked"]
+def toUnicode (s : @&ASCII.String) : Unicode.String :=
+  loop 0 (Nat.zero_le _) ""
+where
+  loop (i : Nat) (hi : i ≤ s.length) (r : Unicode.String) :=
+    if h : i = s.length then r else
+      have hi : i < s.length := Nat.lt_of_le_of_ne hi h
+      loop (i + 1) (Nat.succ_le_of_lt hi) (r.push s[i])
+termination_by loop => s.length - i
+
+instance : Coe ASCII.String Unicode.String where
+  coe := String.toUnicode
+
+instance : ToString ASCII.String where
+  toString s := s.toUnicode
+
+/-- Coerce a Unicode string into an ASCII string -/
+@[extern "lean_string_to_utf8"]
+def ofUnicode (s : @&Unicode.String) (h : s.isASCII) : ASCII.String :=
+  loop s.data (String.all_eq .. ▸ h) (mkEmpty s.length)
+where
+  loop (cs : List Unicode.Char) (h : cs.all Char.isASCII) (acc : ASCII.String) : ASCII.String :=
+    match cs with
+    | [] => acc
+    | c :: cs =>
+      have h := Bool.and_eq_true _ _ ▸ List.all_cons ▸ h
+      loop cs h.2 (acc.push (c.toASCII h.1))
+alias _root_.String.toASCII := ofUnicode
+
+@[inherit_doc String.ofUnicode]
+def ofUnicode? (s : Unicode.String) : Option ASCII.String :=
+  if h : s.isASCII then some (.ofUnicode s h) else none
+alias _root_.String.toASCII? := ofUnicode?
+
+@[inherit_doc String.ofUnicode]
+def ofUnicode! (s : Unicode.String) : ASCII.String :=
+  if h : s.isASCII then .ofUnicode s h else panic! "characters out of ASCII range"
+alias _root_.String.toASCII! := ofUnicode!
 
 open Lean Parser in
 /-- Syntax for ASCII string -/
