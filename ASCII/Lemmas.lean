d01bb7603365ae08703d57996c73ebb6b66972b5
F. G. Dorais <fgdorais@gmail.com>
2024-03-11 19:08:48 -0400

    fix: protected types

--- ASCII/Lemmas.lean
+++ ASCII/Lemmas.lean
@@ -11,7 +11,7 @@ namespace ASCII
 
 namespace Char
 
-theorem toNat_eq_iff_ofNatAux_eq (c : Char) (n : Nat) (hn : n < 128) :
+theorem toNat_eq_iff_ofNatAux_eq (c : ASCII.Char) (n : Nat) (hn : n < 128) :
     c.toNat = n ↔ ofNatAux n hn = c := by
   simp only [toNat, ofNatAux]
   constructor
@@ -21,22 +21,22 @@ theorem toNat_eq_iff_ofNatAux_eq (c : Char) (n : Nat) (hn : n < 128) :
 theorem toNat_ofNatAux (n : Nat) (hn : n < 128) : toNat (ofNatAux n hn) = n := by
   rw [toNat_eq_iff_ofNatAux_eq]
 
-theorem ofNatAux_toNat (c : Char) : ofNatAux c.toNat c.toNat_lt = c := by
+theorem ofNatAux_toNat (c : ASCII.Char) : ofNatAux c.toNat c.toNat_lt = c := by
   rw [← toNat_eq_iff_ofNatAux_eq]
 
-theorem toNat_eq_iff_ofNat_eq (c : Char) (n : Nat) (hn : n < 128) :
+theorem toNat_eq_iff_ofNat_eq (c : ASCII.Char) (n : Nat) (hn : n < 128) :
     c.toNat = n ↔ ofNat n = c := by
   rw [ofNat, dif_pos hn]; exact toNat_eq_iff_ofNatAux_eq ..
 
 theorem toNat_ofNat (n : Nat) (hn : n < 128) : toNat (ofNat n) = n := by
   rw [toNat_eq_iff_ofNat_eq]; exact hn
 
-theorem ofNat_toNat (c : Char) : ofNat c.toNat = c := by
+theorem ofNat_toNat (c : ASCII.Char) : ofNat c.toNat = c := by
   rw [← toNat_eq_iff_ofNat_eq]; exact c.toNat_lt
 
 /-! ### Unicode Conversion -/
 
-theorem toUnicode_eq_iff_ofUnicode_eq (c : Char) (u : Unicode.Char) (hu : u.isASCII) :
+theorem toUnicode_eq_iff_ofUnicode_eq (c : ASCII.Char) (u : Unicode.Char) (hu : u.isASCII) :
     c.toUnicode = u ↔ ofUnicode u hu = c := by
   simp only [Char.toUnicode, ofUnicode]
   constructor
@@ -53,7 +53,7 @@ theorem toUnicode_ofUnicode (u : Unicode.Char) (hu : u.isASCII) :
     Char.toUnicode (ofUnicode u hu) = u := by
   rw [toUnicode_eq_iff_ofUnicode_eq]
 
-theorem ofUnicode_toUnicode (c : Char) :
+theorem ofUnicode_toUnicode (c : ASCII.Char) :
     ofUnicode c.toUnicode c.toUnicode_isASCII = c := by
   rw [← toUnicode_eq_iff_ofUnicode_eq]
 
@@ -65,13 +65,13 @@ theorem ofUnicode!_eq_ofUnicode (u : Unicode.Char) (hu : u.isASCII) :
 
 /-! ### Case Conversion -/
 
-theorem isLower_iff (c : Char) : c.isLower ↔ 97 ≤ c.toNat ∧ c.toNat ≤ 122 := by
+theorem isLower_iff (c : ASCII.Char) : c.isLower ↔ 97 ≤ c.toNat ∧ c.toNat ≤ 122 := by
   simp [isLower]; rfl
 
-theorem isUpper_iff (c : Char) : c.isUpper ↔ 65 ≤ c.toNat ∧ c.toNat ≤ 90 := by
+theorem isUpper_iff (c : ASCII.Char) : c.isUpper ↔ 65 ≤ c.toNat ∧ c.toNat ≤ 90 := by
   simp [isUpper]; rfl
 
-theorem not_isUpper_of_isLower {c : Char} : c.isLower → ¬ c.isUpper := by
+theorem not_isUpper_of_isLower {c : ASCII.Char} : c.isLower → ¬ c.isUpper := by
   simp only [isLower_iff, isUpper_iff]
   intro ⟨hl, _⟩ ⟨_, hu⟩
   apply Nat.lt_irrefl c.toNat
@@ -79,10 +79,10 @@ theorem not_isUpper_of_isLower {c : Char} : c.isLower → ¬ c.isUpper := by
   apply Nat.lt_of_lt_of_le _ hl
   decide
 
-theorem not_isLower_of_isUpper {c : Char} : c.isUpper → ¬ c.isLower :=
+theorem not_isLower_of_isUpper {c : ASCII.Char} : c.isUpper → ¬ c.isLower :=
   flip not_isUpper_of_isLower
 
-theorem not_isUpper_toLower (c : Char) : ¬ c.toLower.isUpper := by
+theorem not_isUpper_toLower (c : ASCII.Char) : ¬ c.toLower.isUpper := by
   simp [-not_and, toLower, isUpper_iff]
   split
   · next h =>
@@ -94,7 +94,7 @@ theorem not_isUpper_toLower (c : Char) : ¬ c.toLower.isUpper := by
     decide
   · assumption
 
-theorem not_isLower_toUpper (c : Char) : ¬ c.toUpper.isLower := by
+theorem not_isLower_toUpper (c : ASCII.Char) : ¬ c.toUpper.isLower := by
   simp [-not_and, toUpper, isLower_iff]
   split
   · next h =>
@@ -106,16 +106,16 @@ theorem not_isLower_toUpper (c : Char) : ¬ c.toUpper.isLower := by
     decide
   · assumption
 
-theorem toUpper_eq_self_of_not_isLower {c : Char} (h : ¬ c.isLower) : c.toUpper = c := by
+theorem toUpper_eq_self_of_not_isLower {c : ASCII.Char} (h : ¬ c.isLower) : c.toUpper = c := by
   simp [toUpper]; intro; contradiction
 
-theorem toLower_eq_self_of_not_isUpper {c : Char} (h : ¬ c.isUpper) : c.toLower = c := by
+theorem toLower_eq_self_of_not_isUpper {c : ASCII.Char} (h : ¬ c.isUpper) : c.toLower = c := by
   simp [toLower]; intro; contradiction
 
-theorem toLower_toLower (c : Char) : c.toLower.toLower = c.toLower :=
+theorem toLower_toLower (c : ASCII.Char) : c.toLower.toLower = c.toLower :=
   toLower_eq_self_of_not_isUpper <| not_isUpper_toLower _
 
-theorem toUpper_toUpper (c : Char) : c.toUpper.toUpper = c.toUpper :=
+theorem toUpper_toUpper (c : ASCII.Char) : c.toUpper.toUpper = c.toUpper :=
   toUpper_eq_self_of_not_isLower <| not_isLower_toUpper _
 
 end Char
